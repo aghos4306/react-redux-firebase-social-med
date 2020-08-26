1187ef7212eb822f63d0cb07bad46bafb1968435
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-08-26 13:43:53 +0300

    Card to render details created on PostDetails component and rendered in App js, each post should have own id

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavigationBar from './Components/Layout/NavigationBar';
 import LogIn from './Components/Auth/Login';
 import Register from './Components/Auth/Register';
+import PostDetails from './Components/Posts/PostDetails';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/login" component={LogIn}></Route>
           <Route exact path="/register" component={Register}></Route>
+          <Route exact path="/post/:id" component={PostDetails}></Route>
         </Switch>
       </div>
     </Router>
---
+++ src/Components/Posts/PostDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PostDetails = () => (
+  <div className="container">
+    <div className="card">
+      <div className="card-title">React Redux Insight</div>
+      <div className="card-content">
+        <p>
+          Redux is a pattern and library for managing and updating application state, using events called "actions". It serves as a centralized store for state that needs to be used across your entire
+          application, with rules ensuring that the state can only be updated in a predictable fashion.
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default PostDetails;
