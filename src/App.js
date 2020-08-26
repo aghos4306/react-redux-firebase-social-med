7bbfba7fc1e3e130fad45e85dc6ceb86736d6f08
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-08-26 13:29:50 +0300

    register form created and rendered in App js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavigationBar from './Components/Layout/NavigationBar';
 import LogIn from './Components/Auth/Login';
+import Register from './Components/Auth/Register';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <NavigationBar />
         <Switch>
           <Route exact path="/login" component={LogIn}></Route>
+          <Route exact path="/register" component={Register}></Route>
         </Switch>
       </div>
     </Router>
---
+++ src/Components/Auth/Register.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Register = () => (
+  <div className="container">
+    <form>
+      <div className="input-field">
+        <input id="firstname" type="text" className="validate" />
+        <label for="firstname">FirstName</label>
+      </div>
+      <div className="input-field">
+        <input id="lastname" type="text" className="validate" />
+        <label for="lastname">LastName</label>
+      </div>
+      <div className="input-field">
+        <input id="email" type="email" className="validate" />
+        <label for="email">Email</label>
+      </div>
+      <div className="input-field">
+        <input id="password" type="password" className="validate" />
+        <label for="password">Password</label>
+      </div>
+      <button className="btn waves-effect waves-light" type="submit" name="action">
+        Register
+      </button>
+    </form>
+  </div>
+);
+
+export default Register;
