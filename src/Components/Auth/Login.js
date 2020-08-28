dc50ce66ae71abb18289450f2e33e192393dc1f9
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-08-28 19:38:25 +0300

    functinality set up on Login js form to capture and console user log in details

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -3,20 +3,41 @@ import React from 'react';
 class LogIn extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      email: null,
+      password: null,
+    };
+
+    this.handleSubmission = this.handleSubmission.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  handleSubmission = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
 
   render() {
     return (
       <div className="container">
-        <form>
+        <form onSubmit={this.handleSubmission}>
           <div className="input-field">
-            <input id="email" type="text" className="validate" />
+            <input id="email" type="text" className="validate" onChange={this.handleChange} />
             <label htmlFor="email">Email:</label>
           </div>
           <div className="input-field">
-            <input id="password" type="password" className="validate" />
+            <input id="password" type="password" className="validate" onChange={this.handleChange} />
             <label htmlFor="password">Password:</label>
           </div>
+          <button className="btn waves-effect waves-light" type="submit" name="action">
+            Login
+          </button>
         </form>
       </div>
     );
