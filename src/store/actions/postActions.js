d3574c30a28249b30c81e4451edbfc02901b841f
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-08-29 13:02:40 +0300

    post Action added and rootReducer updated to accomodate CREATE_POST and POST_ERROR

--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -1,3 +1,16 @@
+import Firebase from 'firebase';
+
+const db = Firebase.firestore();
+
 export const createPost = (post) => {
-  return () => {};
+  return (dispatch) => {
+    db.collection('posts')
+      .add(post)
+      .then(() => {
+        dispatch({ tyype: 'CREATE_POST_SUCCESSFUL' });
+      })
+      .catch((err) => {
+        dispatch({ type: 'CREATE_POST_ERR' }, err);
+      });
+  };
 };
