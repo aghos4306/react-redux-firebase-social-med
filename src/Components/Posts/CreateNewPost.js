e0b8cb991847524f8734a93505fa1b3be4d81b6e
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-08-29 12:23:13 +0300

    createPost action added

--- src/Components/Posts/CreateNewPost.js
+++ src/Components/Posts/CreateNewPost.js
@@ -1,16 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createPost } from '../../store/actions/postActions';
 
 class CreateNewPost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmission = this.handleSubmission.bind(this);
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  handleSubmission = (e) => {
+    e.preventDefault();
+    this.props.createPost({
+      title: this.state.postTitle,
+      content: this.state.postContent,
+    });
+  };
+
   render() {
     return (
       <div className="container">
-        <form>
+        <form onSubmit={this.handleSubmission}>
           <div className="input-field">
-            <input id="postTitle" type="text" className="validate" />
+            <input id="postTitle" type="text" className="validate" onChange={this.handleChange} />
             <label for="postTitle">Title</label>
           </div>
           <div className="input-field">
-            <textarea id="postContent" className="materialize-textarea" data-length="550"></textarea>
+            <textarea id="postContent" className="materialize-textarea" data-length="550" onChange={this.handleChange}></textarea>
             <label for="postContent">Content</label>
           </div>
           <button type="submit" className="btn waves-effect waves-light" name="action">
@@ -22,4 +45,15 @@ class CreateNewPost extends React.Component {
   }
 }
 
-export default CreateNewPost;
+//export default CreateNewPost;
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createPost: (post) => dispatch(createPost(post)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateNewPost);
